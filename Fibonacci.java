eafda07639ae65d1b3f7706e2b6414d92ceeb936
rch <jwlkr01@gmail.com>
2017-02-19 03:50:25 +0000

    Refactored fibIterative() to remove need for array. Created new method fibMem() to use memoization technique, which makes recursive Fibonacci computation efficient by caching values.

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,28 +1,46 @@
 package example.recursion;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Fibonacci {
+
+    private int fibMem(int n) {
+        Map<Integer, Integer> c = new HashMap<>();
+
+        if (n == 0 || n == 1) return n;
+        else if (c.containsKey(n)) return c.get(n);
+        else {
+            c.put(n, fibMem(n - 1) + fibMem(n - 2));
+            return c.get(n);
+        }
+    }
+
     private int fibRecursive(int n) {
         if (n == 0 || n == 1) return n;
         else return fibRecursive(n - 1) + fibRecursive(n - 2);
     }
 
     private int fibIterative(int n) {
-        int [] f = new int[n + 1];
-        f[0] = 0;
-        f[1] = 1;
+        int f0 = 0;
+        int f1 = 1;
+        int fn = 0;
 
         for (int i = 2; i <= n; i++) {
-            f[i] = f[i - 1] + f[i - 2];
+            fn = f0 + f1;
+            f0 = f1;
+            f1 = fn;
         }
 
-        return f[n];
+        return fn;
     }
 
     // tests
     public static void main(String [] args) {
         Fibonacci f = new Fibonacci();
 
-        System.out.println(f.fibRecursive(5));
-        System.out.println(f.fibIterative(5));
+        System.out.println("fibMem: " + f.fibMem(83));
+        System.out.println("fibIterative: " + f.fibIterative(100));
+        System.out.println("fibRecursive: " + f.fibRecursive(80));
     }
 }
